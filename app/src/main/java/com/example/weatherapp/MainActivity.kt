f5d2c65c5668fab3ee70cbc1e38f43011dfbf4cf
Arek908 <arekkasz07@gmail.com>
2025-10-26 17:13:47 +0000

    new background added and short logic

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.weatherapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.graphics.drawable.toDrawable
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +23,24 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val userInput = findViewById<TextInputEditText>(R.id.userInput)
+        val searchBtn = findViewById<Button>(R.id.searchBtn)
+        var container = findViewById<CardView>(R.id.container)
+        var background = findViewById<LinearLayout>(R.id.background)
+
+        val season = "winter"
+
+        searchBtn.setOnClickListener {
+            when(season){
+                "summer" -> background.setBackgroundResource(R.drawable.lato)
+                "winter" -> background.setBackgroundResource(R.drawable.zima)
+                "spring" -> background.setBackgroundResource(R.drawable.wiosna)
+                "autumn" -> background.setBackgroundResource(R.drawable.jesien)
+                else -> background.setBackgroundResource(R.drawable.day)
+            }
+
+            container.visibility = View.VISIBLE
         }
     }
 }
